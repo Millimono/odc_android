16670ccc6baaa9ccda0d4cad6128539cb2d46f97
Millimono <millimono64.sm@gmail.com>
2024-03-12 10:20:59 +0000

    mise a jour

---
+++ app/src/main/java/com/example/odc1/Personne.java
@@ -0,0 +1,20 @@
+package com.example.odc1;
+
+class Personne {
+    int id;
+    String nom, prenom;
+
+    public Personne(int id, String nom, String prenom) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public String getPrenom() {
+        return this.prenom;
+    }
+}
